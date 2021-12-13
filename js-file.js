d210f55784a1b7ff1274b95ca72f54bc8266d49c
Jhevon Tulloch <Mrjtullo@gmail.com>
2021-12-13 18:56:55 -0500

    button clears screen and resizes project to users input

--- js-file.js
+++ js-file.js
@@ -1,5 +1,6 @@
 
-const container = document.querySelector('#container');
+// const container = document.querySelector('#grid-container');
+const container = document.querySelector('.container');
 
 
 function makeRows(rows, cols){
@@ -8,11 +9,26 @@ function makeRows(rows, cols){
     for (c=0; c<(rows * cols); c++){
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
-        cell.addEventListener("mouseover", function bgChange() {
-           const clr = "white";
-            cell.style.backgroundColor = clr;
+        cell.addEventListener("mouseover", function(){
+            const clr = "blue";
+             cell.style.backgroundColor = clr;
+
         });
     };
 };
     
-    makeRows(16, 16);
+    makeRows(16,16);
+
+    const btn = document.querySelector('.butn');
+    
+    btn.addEventListener('click',()=>{
+       let newVal = prompt("how many squares would you like to add (please keep under 100)")
+        container.innerHTML = "";
+        if (newVal > 0 & newVal <= 100){
+        makeRows(newVal, newVal);
+    }   else{
+        alert("Please enter a number between 1 and 100s")
+        makeRows(16,16)
+    }
+    })
+   
